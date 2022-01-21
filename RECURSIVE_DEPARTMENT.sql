27aa11a295211dc6254c66249e96eede2dd288ce
Mahdi <mahdi.sma@gmail.com>
2022-01-21 17:19:39 +0330

    some minor changes in sql syntax

--- RECURSIVE_DEPARTMENT.sql
+++ RECURSIVE_DEPARTMENT.sql
@@ -13,7 +13,7 @@ q0 AS (SELECT DISTINCT pid AS id FROM ..tbldepartments)
 		 INNER JOIN q  ON  q.id =  tbl1.pid AND q.id <> tbl1.id)
 
 ,q1 AS (SELECT * FROM q)
-, q2 AS (SELECT q1.id,q1.idpedar, tbl1.des, tbl2.des AS pedar_des FROM q1 LEFT JOIN ..tblsakhtarunit as tbl1 on tbl1.id = q1.id
+, q2 AS (SELECT q1.id,q1.idpedar, tbl1.des, tbl2.des AS pedar_des FROM q1 LEFT JOIN ..tbldepartments as tbl1 on tbl1.id = q1.id
 		LEFT JOIN ..tbldepartments AS tbl2 ON tbl2.id = q1.idpedar)
 
 SELECT * FROM q2 ORDER BY idpedar
